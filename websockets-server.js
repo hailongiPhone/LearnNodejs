5cbfd7e03a19c28eb038960bb0b2412b5aaa0884
hailong <ghl_hh@hotmail.com>
2017-04-04 02:18:06 +0900

    - 简单聊天室功能， 消息记录，给新登录的客户端发送历史记录，客户端发送消息广播到聊天室中所有客户端

--- websockets-server.js
+++ websockets-server.js
@@ -6,14 +6,29 @@ var ws = new WebSoketServer({
   port: port
 });
 
+// 简单的聊天历史记录
+var messages = [];
+
 console.log('websockets server started');
 
 ws.on('connection', function (socket) {
   console.log('client connection established');
 
+  // 发送历史聊天信息
+  messages.forEach(function (msg){
+    socket.send(msg);
+  });
+
   socket.on('message', function (data) {
     console.log('message received:'+data);
-    socket.send(data);
+    messages.push(data);
+
+    //聊天室广播
+    ws.clients.forEach(function (clientSocket){
+      if (clientSocket != socket){
+        clientSocket.send(data);
+      }
+    });
   });
 
 });
